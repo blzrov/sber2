8254d2d2e4b0522a2c3e9425f3e8e5b39b578f9b
Denis Belozerov <molotov.ant@mail.ru>
2023-08-09 20:14:06 +0500

    init 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Event from "./pages/Event";
 
 export default function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Main</Link>
+              </li>
+              <li>
+                <Link to="/event/1">Event</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/event/:id">
+              <Event />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 }
