ac7bfa4f51c5360f44dd86f1fa6c85d11b032729
Denis Belozerov <molotov.ant@mail.ru>
2023-08-09 20:32:01 +0500

    init 3

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
+import { useState } from "react";
 import "./App.css";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { MantineProvider, ColorSchemeProvider } from "@mantine/core";
 
 import Home from "./pages/Home";
 import Event from "./pages/Event";
 
+import ButtonScheme from "./components/ButtonScheme";
+
 export default function App() {
+  const [colorScheme, setColorScheme] = useState("light");
+
+  const toggleColorScheme = (value) => setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
   return (
-    <div className="App">
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Main</Link>
-              </li>
-              <li>
-                <Link to="/event/1">Event</Link>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route path="/event/:id">
-              <Event />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme }}>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Main</Link>
+                </li>
+                <li>
+                  <Link to="/event/1">Event</Link>
+                </li>
+                <li>
+                  <ButtonScheme />
+                </li>
+              </ul>
+            </nav>
+            <Switch>
+              <Route path="/event/:id">
+                <Event />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
